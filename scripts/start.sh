9325c8fb7abc78e4118afbe0c7a4acf8512047db
9rin <129544574+9rin@users.noreply.github.com>
2025-05-28 00:21:03 +0900

    start.sh 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
+
 PROJECT_ROOT="/home/ubuntu/toytweet"
-JAR_FILE="$PROJECT_ROOT/demo-0.0.1-SNAPSHOT.jar"
+JAR_SOURCE=$(ls $PROJECT_ROOT/build/libs/*.jar | head -n 1)
+JAR_TARGET="$PROJECT_ROOT/demo-0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
-TIME_NOW=$(date +%c)# build 파일 복사
-echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+TIME_NOW=$(date +%c)
 
-# jar 파일 실행
-echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG2> $ERROR_LOG &
+echo "$TIME_NOW > JAR 복사 시작: $JAR_SOURCE -> $JAR_TARGET" >> $DEPLOY_LOG
+cp $JAR_SOURCE $JAR_TARGET
 
-CURRENT_PID=$(pgrep -f $JAR_FILE)echo "$TIME_NOW > 실행된 프로세스의 아이디는 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+echo "$TIME_NOW > JAR 실행 시작" >> $DEPLOY_LOG
+nohup java -jar $JAR_TARGET > $APP_LOG 2> $ERROR_LOG &
+
+CURRENT_PID=$(pgrep -f $JAR_TARGET)
+echo "$TIME_NOW > 실행된 프로세스의 아이디는 $CURRENT_PID 입니다." >> $DEPLOY_LOG
