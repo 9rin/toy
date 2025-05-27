25d819f5ac623857f2fb7a00181153322cc8a6f4
9rin <129544574+9rin@users.noreply.github.com>
2025-05-28 01:54:21 +0900

    start.sh 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,15 +2,9 @@
 
 PROJECT_ROOT="/home/ubuntu/toytweet"
 JAR_PATH="$PROJECT_ROOT/demo-0.0.1-SNAPSHOT.jar"
-
-APP_LOG="$PROJECT_ROOT/application.log"
-ERROR_LOG="$PROJECT_ROOT/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
-TIME_NOW=$(date +%c)
+echo "$(date +%c) > JAR 직접 실행 (포그라운드 모드)" >> $DEPLOY_LOG
 
-echo "$TIME_NOW > JAR 실행 시작" >> $DEPLOY_LOG
-nohup java -jar -Dspring.config.location=$PROJECT_ROOT/application.yml $JAR_PATH > $APP_LOG 2> $ERROR_LOG &
-
-CURRENT_PID=$(pgrep -f $JAR_PATH)
-echo "$TIME_NOW > 실행된 프로세스의 아이디는 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+# ✅ 백그라운드 실행 없이 직접 실행
+java -jar -Dspring.config.location=$PROJECT_ROOT/application.yml $JAR_PATH
