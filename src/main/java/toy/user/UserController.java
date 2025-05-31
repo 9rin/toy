9fc94f768b167261c48b9417156ffa726dd871fc
9rin <129544574+9rin@users.noreply.github.com>
2025-06-01 00:39:04 +0900

    CORS 에러 코드 다시 뺌

--- src/main/java/toy/user/UserController.java
+++ src/main/java/toy/user/UserController.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "https://twitter-clone-tawny-phi.vercel.app")
+//@CrossOrigin(origins = "https://twitter-clone-tawny-phi.vercel.app")
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
