02b971fb95179619053bf917f06e783a04f56f58
9rin <129544574+9rin@users.noreply.github.com>
2025-06-02 00:20:49 +0900

    response body 수정

--- src/main/java/toy/tweet/dto/response/TweetResponseDto.java
+++ src/main/java/toy/tweet/dto/response/TweetResponseDto.java
@@ -11,7 +11,8 @@ import java.time.LocalDateTime;
 @Builder
 public class TweetResponseDto {
 
-    // 표시할 것: 닉네임, 핸들, 트윗 내용, 트윗 생성일자
+    // 표시할 것: ID, 닉네임, 핸들, 트윗 내용, 트윗 생성일자
+    private Long userId;
     private String nickname;
     private String handle;
     private String content;
@@ -20,6 +21,7 @@ public class TweetResponseDto {
     // Build
     public static TweetResponseDto from(Tweet tweet){
         return TweetResponseDto.builder()
+                .userId(tweet.getId())
                 .nickname(tweet.getUser().getNickname())
                 .handle(tweet.getUser().getHandle())
                 .content(tweet.getContent())
