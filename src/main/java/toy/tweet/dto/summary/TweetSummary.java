d5424c2548fa59cf2f4cb63e2d407b16b4f8b75c
9rin <129544574+9rin@users.noreply.github.com>
2025-06-02 00:29:28 +0900

    response body 수정

--- src/main/java/toy/tweet/dto/summary/TweetSummary.java
+++ src/main/java/toy/tweet/dto/summary/TweetSummary.java
@@ -5,10 +5,11 @@ import toy.user.User;
 
 import java.time.LocalDateTime;
 
-public record TweetSummary(Long tweetId, String nickname, String handle, String content, LocalDateTime createdAt) {
+public record TweetSummary(Long tweetId, Long userId, String nickname, String handle, String content, LocalDateTime createdAt) {
     public static TweetSummary from(Tweet tweet) {
         return new TweetSummary(
                 tweet.getId(),
+                tweet.getUser().getUserId(),
                 tweet.getUser().getNickname(),
                 tweet.getUser().getHandle(),
                 tweet.getContent(),
